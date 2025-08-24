f1c468ed1f0ab24a9d46ad5dbe6a9beb77ea0658
Kasis11 <21cp032@bvmengineering.ac.in>
2025-08-24 14:07:40 +0530

    updated file serializers

--- expense_tracker_backend/core/serializers.py
+++ expense_tracker_backend/core/serializers.py
@@ -46,10 +46,4 @@ class RegisterSerializer(serializers.ModelSerializer):
         return user
 
 
-    def create(self, validated_data):
-        user = User.objects.create_user(
-            username = validated_data['username'],
-            email = validated_data['email'],
-            password = validated_data['password']
-        )
-        return user
+    
